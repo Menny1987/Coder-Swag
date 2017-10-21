157ee5a0784dd015975f165e635594083bebdec3
Menny1987 <Mennyyyy@gmail.com>
2017-10-22 00:42:16 +0300

    Creating IBOutlets

---
+++ Coder-Swag/Coder-Swag/Controller/CategoriesVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Coder-Swag
+//
+//  Created by Menny Atia on 22/10/2017.
+//  Copyright © 2017 Menny Atia. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+  
+
+
+}
+
